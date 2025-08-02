7064f82fd4262cb34d039c25fc670c267563a309
dipen-dra <dipendrajr999@gmail.com>
2025-08-02 23:03:31 +0545

    feat :gyro sensor

--- lib/core/common/dashboard_sensor.dart
+++ lib/core/common/dashboard_sensor.dart
@@ -4,11 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:motofix_app/app/cubit/bottom_navigation_cubit.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
-/// A dedicated class to handle gyroscope logic for dashboard navigation.
 class DashboardGyroHandler {
   final BuildContext context;
 
-  // Private subscription and flag for gyroscope logic.
   StreamSubscription? _gyroSub;
   bool _gyroTriggered = false;
 
@@ -16,33 +14,28 @@ class DashboardGyroHandler {
     required this.context,
   });
 
-  /// Starts listening to the gyroscope event stream.
   void startListening() {
     _gyroSub = gyroscopeEvents.listen(_onGyroscope);
   }
 
-  /// Disposes of the stream subscription to prevent memory leaks.
+
   void dispose() {
     _gyroSub?.cancel();
   }
 
-  /// Handles rotation events from the gyroscope to switch tabs.
   void _onGyroscope(GyroscopeEvent event) {
     const rotationThreshold = 4.0;
 
-    // Check for a rapid rotation and ensure the trigger is not on cooldown.
     if (!_gyroTriggered &&
         (event.x.abs() > rotationThreshold ||
             event.y.abs() > rotationThreshold)) {
-      _gyroTriggered = true; // Set debounce flag to prevent multiple triggers
+      _gyroTriggered = true; 
 
-      // Access the navigation cubit from the context.
       final cubit = context.read<BottomNavigationCubit>();
       final currentIndex = cubit.state;
       const screenCount = 4; // The total number of tabs.
       final nextIndex = (currentIndex + 1) % screenCount;
 
-      // Command the cubit to change the tab.
       cubit.changeTab(nextIndex);
 
       // Provide visual feedback to the user.
